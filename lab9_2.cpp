4c4d87058a33e3afc956cf2106c778b4b18d9558
BestChennn <bestchenbestchen@gmail.com>
2022-12-23 16:12:03 +0700

    Complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 #include<iomanip>
 #include<string>
+#include<string>
+#include<cmath>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double p){
+
+    if(p>90){
+       p = 'A';
+
+    }else if(p<=90 && p>75){
+        p = 'B';
+    }else if(p<=75 && p>60){
+        p = 'C';
+    }else if(p<=60 && p> 45){
+        p ='D';
+    }else if(p<=45){
+        p ='F';
+    }
+
+    return p;
+
+}
 
 int main(){
 	//Input the number of students
@@ -17,12 +36,17 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin>> score[i];
 		i++;
 	}
 	
+
+
+
+
+
 	//Print names scores and grades
 	i = 0;
 	cout << "---------------------------------------------\n";
